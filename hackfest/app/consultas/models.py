e7abf7e27cd61e0250f8e88426bfb6c811be1344
Kendal Sosa <kendal.alfonso37@gmail.com>
2017-11-19 07:30:37 -0600

    Arreglo a modelos de consulta, cosita pequenia

--- hackfest/app/consultas/models.py
+++ hackfest/app/consultas/models.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-from app.gamificacion.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 # Create your models here.
 
 class Proyecto(models.Model):
