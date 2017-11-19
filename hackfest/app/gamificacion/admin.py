1914231c98f6c9e8537f3236578db6ecf9d0740e
Kendal Sosa <kendal.alfonso37@gmail.com>
2017-11-19 08:06:40 -0600

    Editado el admin.py de gamificacion para registrar recompensas

--- hackfest/app/gamificacion/admin.py
+++ hackfest/app/gamificacion/admin.py
@@ -2,10 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import User
+from models import User, Recompensa
 
 class UserAdmin(admin.ModelAdmin):
 	list_display=('username','nivel','puntuacion','recompensa','last_name','email',)
 
 
 admin.site.register(User,UserAdmin)
+admin.site.register(Recompensa)
